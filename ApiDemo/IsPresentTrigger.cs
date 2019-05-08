fec361829d0e15ccccff2ac72a6b71451a0b0531
Tony Champion <tonyc@championdatasolutions.com>
2019-05-08 09:24:40 -0500

    Added APIDemo

---
+++ ApiDemo/IsPresentTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace ApiDemo
+{
+    class IsPropertyPresentTrigger : StateTriggerBase
+    {
+        public string TypeName { get; set; }
+        public string PropertyName { get; set; }
+
+        private Boolean _isPresent;
+        private bool? _isPropertyPresent = null;
+
+        public Boolean IsPresent
+        {
+            get { return _isPresent; }
+            set
+            {
+                _isPresent = value;
+                if (_isPropertyPresent == null)
+                {
+                    // Call into ApiInformation method to determine if property is present.
+                    //_isPropertyPresent =
+                    //ApiInformation.IsPropertyPresent(TypeName, PropertyName);
+                }
+
+                // If the property presence matches _isPresent then the trigger will be activated;
+                SetActive(_isPresent == _isPropertyPresent);
+            }
+        }
+    }
+}
